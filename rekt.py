51d0974e603890f2606cef8a8ccd311f09b7ce92
FwzNk <68651103+FwzNk@users.noreply.github.com>
2021-07-23 16:07:59 +0800

    Update rekt.py

--- rekt.py
+++ rekt.py
@@ -3401,7 +3401,7 @@ def down_it(item):
 				print("\033[91mshut<->down\033[0m")
 			time.sleep(.1)
 	except socket.error as e:
-		print("\033[91mno connection! server maybe rekt\033[0m")
+		print("\033[91mno connection! server maybe rekt-ed\033[0m")
 		#print("\033[91m",e,"\033[0m")
 		time.sleep(.1)
 
